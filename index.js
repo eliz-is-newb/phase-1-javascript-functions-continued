f911d744b58a486c9b2470388df7e02d777c5a38
elizabeth sky degrate <skydegrate@gmail.com>
2022-07-19 09:22:32 -0400

    commit tree d03f4a1714f17593ae7ad395c944c0f9c5a61370

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(funActivity="roller-skate") {
+    return `This Saturday, I want to ${funActivity}!`;
+}
+
+function mondayWork(workActivity="go to the office") {
+    return `This Monday, I will ${workActivity}.`;
+}
+
+function wrapAdjective(party="*") {
+    return function cowboys(which="special") {
+        return `You are ${party}${which}${party}!`;
+    };
+
+}
+wrapAdjective("%")("a dedicated programmer");
